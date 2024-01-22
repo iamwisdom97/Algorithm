5c71b611b277b5d8beb8183776fe2cf239fe97af
wisdom <124278045+iamwisdom97@users.noreply.github.com>
2024-01-22 23:15:30 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = {};
+        
+        answer = Arrays.copyOfRange(numbers, num1, num2+1);
+        
+        return answer;
+    }
+}
