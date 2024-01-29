382726fc51a11d3934ff47496ddf7ff5887c477c
wisdom <124278045+iamwisdom97@users.noreply.github.com>
2024-01-29 20:58:01 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.27 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+
+class Solution {
+    public int solution(int price) {
+        int answer = 0;
+        
+        if(price >= 500000){
+            answer = (int) Math.floor(price*0.8);
+        }else if(price >= 300000){
+            answer = (int) Math.floor(price*0.9);
+        }else if(price >= 100000){
+            answer = (int) Math.floor(price*0.95);
+        }else{
+            answer = price;
+        }
+        
+        return answer;
+    }
+}
