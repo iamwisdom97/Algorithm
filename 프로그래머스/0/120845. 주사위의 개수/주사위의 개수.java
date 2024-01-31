969d8e6c1d58f82b5cdee98b60edfb608a10a3c5
wisdom <124278045+iamwisdom97@users.noreply.github.com>
2024-01-31 21:15:10 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 69.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] box, int n) {
+        int answer = 0;
+        
+        answer = (box[0]/n) * (box[1]/n) * (box[2]/n);
+        
+        
+        return answer;
+    }
+}
