a52255aa86a9fde788fcd921da531ce425f1d54d
wisdom <124278045+iamwisdom97@users.noreply.github.com>
2024-02-06 22:45:37 +0900

    [level 0] Title: 대문자와 소문자, Time: 18.54 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for(char c : my_string.toCharArray()){
+            if(Character.isUpperCase(c)){
+                answer += Character.toLowerCase(c) ;
+            }else{
+                answer += Character.toUpperCase(c);
+            }
+        }
+        
+        return answer;
+    }
+}
