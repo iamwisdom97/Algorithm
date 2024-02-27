41bde6a459bb47c3625dd4846035d707b52d9239
wisdom <124278045+iamwisdom97@users.noreply.github.com>
2024-02-27 21:10:34 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 2;
+        
+        for(int i=1; i<=n; i++){
+            if(i*i == n){
+                answer = 1;
+            }
+        }
+        
+        return answer;
+    }
+}
